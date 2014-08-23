3c9010f226933426db03c4629542169fe558839b
prayagupd <prayag.upd@gmail.com>
2014-08-24 00:27:53 +0545

    ADD sliding menu for dans-le-ville

---
+++ app/src/main/java/epistle/pseudononymous/drawer/FindShopFragment.java
@@ -0,0 +1,23 @@
+package epistle.pseudononymous.drawer;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import info.androidhive.slidingmenu.R;
+
+public class FindShopFragment extends Fragment {
+	
+	public FindShopFragment(){}
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_find_people, container, false);
+         
+        return rootView;
+    }
+}
